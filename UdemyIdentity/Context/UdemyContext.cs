bc38ab6ca6ad97e15a89f3967267325965d04376
furkant35 <furkant_35@hotmail.com>
2021-07-04 02:30:13 +0300

    Identity Project
    Identity Project with Asp.net Core

---
+++ UdemyIdentity/Context/UdemyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyIdentity.Context
+{
+    public class UdemyContext : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-2K4U56S\\SQLEXPRESS;database=UdemyIdentity;integrated security=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+      
+    }
+}
